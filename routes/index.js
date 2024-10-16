19e06a48d5ad63bbbbdf5b9ee9d3dad45cdcfe8a
mini-360 <rajabhay56999@gmail.com>
2024-10-16 23:42:44 +0530

    logout functionality created

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,16 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   let error = req.flash("error");
-  res.render("index", { error });
+  res.render("index", { error,loggedin:false });
 });
 
 router.get("/shop", isLoggedIn, async (req, res) => {
   let products = await Product.find();
   res.render("shop", { products });
 });
+
+router.get("/logout", isLoggedIn, (req, res) => {
+  res.render("shop")
+})
 
 export const indexRouter = router;
